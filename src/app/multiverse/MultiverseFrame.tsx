6082d0b2b3bce957a2e5e032c759e6bd74ed7dd5
Nghia Cao <nghiacvt81@gmail.com>
2026-09-05 20:53:26 +0700

    Point Multiverse route to dragon relief art v3.01

--- src/app/multiverse/MultiverseFrame.tsx
+++ src/app/multiverse/MultiverseFrame.tsx
@@ -3,7 +3,7 @@
 export default function MultiverseFrame() {
   return (
     <iframe
-      src="/multiverse/adventure-v300.html?v=300"
+      src="/multiverse/adventure-v300.html?v=301"
       title="Multiverse Adventures"
       style={{ width: "100%", height: "100%", border: 0, display: "block" }}
       allow="fullscreen"
