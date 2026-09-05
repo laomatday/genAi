d2c13fe28f1a382a7a9189f960f146bd1e601328
Nghia Cao <nghiacvt81@gmail.com>
2026-09-05 22:06:36 +0700

    Trigger Multiverse v3.02 exact platform deployment

--- src/app/multiverse/MultiverseFrame.tsx
+++ src/app/multiverse/MultiverseFrame.tsx
@@ -3,7 +3,7 @@
 export default function MultiverseFrame() {
   return (
     <iframe
-      src="/multiverse/adventure-v300.html?v=302"
+      src="/multiverse/adventure-v300.html?v=302b"
       title="Multiverse Adventures"
       style={{ width: "100%", height: "100%", border: 0, display: "block" }}
       allow="fullscreen"
