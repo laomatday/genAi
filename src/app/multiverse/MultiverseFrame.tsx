94eae849475f93ff11baa3b0ea013a4d0e3df2cc
Nghia Cao <nghiacvt81@gmail.com>
2026-09-05 17:57:03 +0700

    Point Multiverse to performance v1.32

--- src/app/multiverse/MultiverseFrame.tsx
+++ src/app/multiverse/MultiverseFrame.tsx
@@ -3,7 +3,7 @@
 export default function MultiverseFrame() {
   return (
     <iframe
-      src="/multiverse/visual-shell.html?v=131"
+      src="/multiverse/visual-shell.html?v=132"
       title="Multiverse Adventures"
       style={{ width: "100%", height: "100%", border: 0, display: "block" }}
       allow="fullscreen"
