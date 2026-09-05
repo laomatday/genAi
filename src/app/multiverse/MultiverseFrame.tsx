e2e8b75cb16eddfa11bebfb4a48401c4aea6244c
Nghia Cao <nghiacvt81@gmail.com>
2026-09-05 22:18:25 +0700

    Point Multiverse to pixel-clean v3.03

--- src/app/multiverse/MultiverseFrame.tsx
+++ src/app/multiverse/MultiverseFrame.tsx
@@ -3,7 +3,7 @@
 export default function MultiverseFrame() {
   return (
     <iframe
-      src="/multiverse/adventure-v300.html?v=302b"
+      src="/multiverse/adventure-v300.html?v=303"
       title="Multiverse Adventures"
       style={{ width: "100%", height: "100%", border: 0, display: "block" }}
       allow="fullscreen"
