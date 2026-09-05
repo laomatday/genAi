1b108fbe2742f005f32b5d937d46545200043dcb
Nghia Cao <nghiacvt81@gmail.com>
2026-09-05 20:24:06 +0700

    Point Multiverse to dungeon expedition v3.00

--- src/app/multiverse/MultiverseFrame.tsx
+++ src/app/multiverse/MultiverseFrame.tsx
@@ -3,7 +3,7 @@
 export default function MultiverseFrame() {
   return (
     <iframe
-      src="/multiverse/game-v201.html?v=art-restored-20260905"
+      src="/multiverse/adventure-v300.html?v=300"
       title="Multiverse Adventures"
       style={{ width: "100%", height: "100%", border: 0, display: "block" }}
       allow="fullscreen"
